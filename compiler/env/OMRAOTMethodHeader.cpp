1b9e14ae36f8eed98a97921b72fb64f74b14e157
Georgiy Krylov <georgiy.krylov@unb.ca>
2020-08-12 11:58:39 -0300

    OMR AOT: Introducing AOTMethodHeader class hierarchy
    This commit brings in the following files: OMRAotMethodHeader,
    OMRAotMethodHeader.cpp, AOTMethodHeader.cpp. The AOTMethodHeader files
    are extensible class hierarchy proposed to be used for serialization and
    deserialization of relocations buffer to and from the storage, alongside
    of the compiled code. This class is supposed to be extended for caching
    more data on the level of compiled code.
    
    Signed-off-by: Georgiy Krylov <georgiy.krylov@unb.ca>

---
+++ compiler/env/OMRAOTMethodHeader.cpp
@@ -0,0 +1,111 @@
+/*******************************************************************************
+ * Copyright (c) 2020, 2020 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+#include "env/AOTMethodHeader.hpp"
+#include <stdlib.h>
+#include <string.h>
+
+TR::AOTMethodHeader* OMR::AOTMethodHeader::self()
+   {
+   return reinterpret_cast<TR::AOTMethodHeader*> (this);
+   }
+
+void OMR::AOTMethodHeader::serialize(uint8_t* buffer)
+   {
+   uintptr_t allocSize = self()->sizeOfSerializedVersion();
+
+   uint8_t* ptr = buffer;
+
+   // Copy the size of the header to the buffer
+   memcpy(ptr,&allocSize,sizeof(uintptr_t));
+
+   // Skip the bytes just copied
+   ptr += sizeof(uintptr_t);
+
+   // Copy the size of compiled code to the buffer
+   memcpy(ptr,&self()->compiledCodeSize,sizeof(uint32_t));
+   
+   // Skip the bytes just copied
+   ptr += sizeof(uint32_t);   
+
+   // Copy the compiled code to the buffer
+   memcpy(ptr,self()->compiledCodeStart,self()->compiledCodeSize);
+
+   // Skip the bytes just copied
+   ptr += self()->compiledCodeSize;
+
+   // Copy the size of relocations data to the buffer
+   memcpy(ptr,&self()->relocationsSize,sizeof(uint32_t));
+
+   // Skip the bytes just copied
+   ptr += sizeof(uint32_t);
+   // Copy the relocations data to the buffer   
+   memcpy(ptr,self()->relocationsStart,self()->relocationsSize);
+   // Now the buffer contains all the data relevant to the
+   // method header.
+   }
+
+OMR::AOTMethodHeader::AOTMethodHeader(uint8_t* rawData)
+   {
+   // This is an examplar implementation of a deserializing
+   // code for a raw data received from the SharedCache message
+   // The first sizeof(uintptr_t) bytes
+   // stores the size of the AOTMethodHeader , we can
+   // just skip them here, they are used by the cache
+   rawData += sizeof(uintptr_t);
+
+   // The next sizeof(uint32_t) bytes contain the 
+   // size of compiled code
+   self()->compiledCodeSize = *((uint32_t*) rawData);
+   rawData += sizeof(uint32_t);
+
+   // Right after the compliedCodeSize, the codeSize of bytes
+   // are the code itself, we should record that as a part
+   // of a header
+   self()->compiledCodeStart = rawData;
+
+   // We should skip size of compiled code in bytes
+   // to get to next element of the header
+   rawData += compiledCodeSize;
+
+   // That is relocation size, occupying the next uint32_t bytes
+   self()->relocationsSize = *((uint32_t*) rawData); 
+
+   // We should skip the sizeof(uint32_t) in bytes to get
+   // to the relocations information
+   rawData += sizeof(uint32_t);
+
+   // We take note of the relocationsStart to use the 
+   // relocations related information in future
+   self()->relocationsStart = rawData;   
+   }
+
+uintptr_t OMR::AOTMethodHeader::sizeOfSerializedVersion()
+   {
+   // An examplar computation of size  of serialized version
+   // of an AOTMethodHeader, required for memory allocation purposes
+   // The size should generally be composed of 
+   // A uintptr_t to hold whole message size,
+   // 2 elements of type uint32_t to store the size of the compiled code and
+   // relocation data
+   // compiledCodeSize of bytes (uint8_t) for compiledcode,
+   // RelocationsSize of bytes (uint8_t) for relocation data.
+   return sizeof(uintptr_t) + 2*sizeof(uint32_t)+self()->compiledCodeSize+self()->relocationsSize;
+   }
---
+++ compiler/env/OMRAOTMethodHeader.hpp
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2020, 2020 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+#ifndef OMR_AOT_METHOD_HEADER
+#define OMR_AOT_METHOD_HEADER
+
+#ifndef OMR_AOTMETHODHEADER_CONNECTOR
+#define OMR_AOTMETHODHEADER_CONNECTOR
+namespace OMR { class AOTMethodHeader; }
+namespace OMR { typedef OMR::AOTMethodHeader AOTMethodHeaderConnector; }
+#endif
+
+#include "infra/Annotations.hpp"
+#include "env/jittypes.h"
+
+namespace TR { class AOTMethodHeader;}
+namespace OMR{
+
+
+class OMR_EXTENSIBLE AOTMethodHeader
+   {
+   /** 
+    * at compile time, the constructor runs with four arguments, 
+    * compiledCodeStart,  compiledCodeSize,  and relocationsStart, relocationsSize
+    * at load time we don't know anything, so we run constructor with no 
+    * parameters and the values from cache are derived.
+    * This is one possible implementation, for a cache with contiguous
+    * code and relocations data stored along them
+    */
+public:
+   TR::AOTMethodHeader* self();
+   AOTMethodHeader(){};
+   AOTMethodHeader(uint8_t* compiledCodeStart, uint32_t compiledCodeSize, uint8_t* relocationsStart, uint32_t relocationsSize):
+      compiledCodeStart(compiledCodeStart),
+      compiledCodeSize(compiledCodeSize),
+      relocationsStart(relocationsStart),
+      relocationsSize(relocationsSize)
+      {};
+   // This constructor could be used for deserialization
+   AOTMethodHeader(uint8_t* rawData);
+   uint8_t* compiledCodeStart;
+   uint32_t compiledCodeSize;
+   uint8_t* relocationsStart;
+   uint32_t relocationsSize;
+   /**
+    * @brief Serializes contents of the AOT Method Header
+    * to a specified memory location
+    * @param buffer 
+    * The memory location to serialize AOTMethodHeader into
+    */
+   void serialize(uint8_t* buffer);
+   uintptr_t sizeOfSerializedVersion();
+   };
+}
+#endif
