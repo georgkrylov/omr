787f3147b7145372869aef970f349915e6c03eee
Georgiy Krylov <georgiy.krylov@unb.ca>
2021-02-09 21:49:23 -0400

    Must have

--- compiler/codegen/OMRCodeGenerator.cpp
+++ compiler/codegen/OMRCodeGenerator.cpp
@@ -1824,6 +1824,7 @@ OMR::CodeGenerator::allocateCodeMemoryInner(
       self()->comp()->setRelocatableMethodCodeStart(warmCode);
       self()->switchCodeCacheTo(codeCache);
       }
+   self()->comp()->setRelocatableMethodCodeStart(warmCode+4);
 
    if (warmCode == NULL)
       {
